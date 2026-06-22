ee756bb88dfc8d882dd98d14d5b2e5335a3b8d64
sorsenya <sorsenya@gmail.com>
2026-06-22 13:28:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbzyI_LIRHkevzMHYhI26zc3Ep2KzkX4xYG132H-w6n5M1SrnHoOlb2KWt2ykejDtZeOfA/exec";
+const API_URL = "./debt.json";
 
 fetch(API_URL)
 .then(response => response.json())
 .then(data => {
 
-    document.getElementById("title").textContent =
-        data._title || "Задолженности СНТ";
-
-    const plots =
-        document.getElementById("plots");
+    const title = document.getElementById("title");
+    const plots = document.getElementById("plots");
 
     plots.innerHTML = "";
 
+    // если есть заголовок — используем, если нет — ставим дефолт
+    if (data._title) {
+        title.textContent = data._title;
+    } else {
+        title.textContent = "Список участков";
+    }
+
     Object.keys(data)
-    .filter(key => key !== "_title")
-    .sort((a,b) => Number(a) - Number(b))
-    .forEach(plot => {
+        .filter(key => key !== "_title")
+        .sort((a, b) => Number(a) - Number(b))
+        .forEach(plot => {
 
-        const button =
-            document.createElement("button");
+            const button = document.createElement("button");
 
-        button.className = "plot-btn";
+            button.className = "plot-btn";
+            button.textContent = plot;
 
-        button.textContent =
-             plot;
+            button.onclick = () => {
+                location.href = "./debt.html?id=" + plot;
+            };
 
-        button.onclick = () => {
-
-            location.href =
-                "debt.html?id=" + plot;
-
-        };
-
-        plots.appendChild(button);
-
-    });
+            plots.appendChild(button);
+        });
 
 })
 .catch(error => {
 
     console.error(error);
 
-    document.getElementById("plots")
-        .innerHTML =
+    document.getElementById("plots").innerHTML =
         "Ошибка загрузки данных";
-
 });
